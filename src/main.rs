3c06a184da28e33669916635700322591d0847ce
zzwtsy <zzwtsy@yumdeb.top>
2024-07-16 03:24:48 +0800

    feat(server-info): 重构main函数和SystemInfo结构体，增加网络接口枚举
    - 引入tokio库的main函数异步执行模型。
    - 在SystemInfo结构体中添加上传时间字段。
    - 重新组织和命名字段以更好地反映其代表的信息。
    - 利用local_ip_address库获取并展示网络接口和IP地址信息。

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,34 @@
-use std::{collections::HashSet, time::Duration};
+use std::{
+    collections::HashSet,
+    time::{Duration, SystemTime, UNIX_EPOCH},
+};
 
-use dto::Host;
+use local_ip_address::list_afinet_netifas;
 use sysinfo::{CpuRefreshKind, Disks, Networks, RefreshKind, System};
 
-use crate::dto::State;
+use dto::{server_info::SystemInfo, Host, State};
 
 mod dto;
 
 pub const VERSION: &'static str = include_str!(concat!(env!("OUT_DIR"), "/VERSION"));
 
-fn main() {
-    let disks = Disks::new_with_refreshed_list();
+#[tokio::main]
+async fn main() {
+    let mut disks = Disks::new_with_refreshed_list();
     let mut sys =
         System::new_with_specifics(RefreshKind::new().with_cpu(CpuRefreshKind::everything()));
-    let mut net_works = Networks::new_with_refreshed_list();
-    loop {
-        net_works.refresh_list();
-        sys.refresh_memory();
-        sys.refresh_cpu_usage();
-        get_os_data(&sys, &disks, &net_works);
-        std::thread::sleep(Duration::from_secs(1));
-    }
+    let mut networks = Networks::new_with_refreshed_list();
+    // loop {
+    disks.refresh_list();
+    sys.refresh_memory();
+    sys.refresh_cpu();
+    networks.refresh_list();
+    get_os_info(&sys, &disks, &networks);
+    std::thread::sleep(Duration::from_secs(1));
+    // }
 }
 
-fn get_os_data(sys: &System, disks: &Disks, net_works: &Networks) {
+fn get_os_info(sys: &System, disks: &Disks, networks: &Networks) {
     let disk_total = disks
         .list()
         .iter()
@@ -34,22 +39,22 @@ fn get_os_data(sys: &System, disks: &Disks, net_works: &Networks) {
         .iter()
         .map(|disk| disk.total_space() - disk.available_space())
         .sum::<u64>();
-    let net_in_transfer = net_works
+    let net_in_transfer = networks
         .list()
         .iter()
         .map(|(_, net)| net.total_received())
         .sum::<u64>();
-    let net_out_transfer = net_works
+    let net_out_transfer = networks
         .list()
         .iter()
         .map(|(_, net)| net.total_transmitted())
         .sum::<u64>();
-    let net_in_speed = net_works
+    let net_in_speed = networks
         .list()
         .iter()
         .map(|(_, net)| net.received())
         .sum::<u64>();
-    let net_out_speed = net_works
+    let net_out_speed = networks
         .list()
         .iter()
         .map(|(_, net)| net.transmitted())
@@ -59,13 +64,17 @@ fn get_os_data(sys: &System, disks: &Disks, net_works: &Networks) {
         .iter()
         .map(|cpu| cpu.brand().to_string())
         .collect::<HashSet<String>>();
+    let network_interfaces = list_afinet_netifas().unwrap();
+    network_interfaces
+        .iter()
+        .for_each(|(name, ip)| println!("{} - {}", name, ip));
     let host = Host {
-        platform: System::name().unwrap_or_default().trim().to_string(),
+        os_name: System::name().unwrap_or_default().trim().to_string(),
         long_os_version: System::long_os_version()
             .unwrap_or_default()
             .trim()
             .to_string(),
-        platform_version: System::os_version().unwrap_or_default(),
+        os_version: System::os_version().unwrap_or_default(),
         cpu,
         cpu_cores: sys.cpus().len() as u64,
         kernel_version: System::kernel_version().unwrap_or_default(),
@@ -74,7 +83,8 @@ fn get_os_data(sys: &System, disks: &Disks, net_works: &Networks) {
         swap_total: sys.total_swap(),
         arch: System::cpu_arch().unwrap_or_default(),
         boot_time: System::boot_time(),
-        country_code: "cn".to_string(),
+        ip_v4: "".to_string(),
+        ip_v6: "".to_string(),
         agent_version: VERSION.to_string(),
     };
     let state = State {
@@ -86,11 +96,18 @@ fn get_os_data(sys: &System, disks: &Disks, net_works: &Networks) {
         net_out_transfer,
         net_in_speed,
         net_out_speed,
-        uptime: System::uptime(),
         load1: System::load_average().one,
         load5: System::load_average().five,
         load15: System::load_average().fifteen,
     };
-    println!("{:}", serde_json::to_string_pretty(&host).unwrap());
-    println!("{:}", serde_json::to_string_pretty(&state).unwrap());
+    let server_info = SystemInfo {
+        host,
+        state,
+        upload_time: SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .expect("Time went backwards")
+            .as_secs()
+            .to_string(),
+    };
+    println!("{:}", serde_json::to_string_pretty(&server_info).unwrap());
 }
